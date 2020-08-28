e1f7c6be19eea2c9f96d43d95fda561378e892ed
Mithun <mitbusiness15@gmail.com>
2020-08-29 02:10:46 +0530

    C

---
+++ C-Programs/Array_reversal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int n,i,a;
+scanf("%d",&n);
+int *arr = (int*)malloc(n * sizeof(int));
+for (i=0;i<n;i++){
+scanf("%d",&arr[i]);
+}
+for(i=n-1;i>=0;i--){
+printf("%d ",(arr[i]));
+}
+}
