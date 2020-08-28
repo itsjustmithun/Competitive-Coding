5dd28c4a14b0fbb90f0f19c716f8882536a54981
Mithun <mitbusiness15@gmail.com>
2020-08-29 02:10:11 +0530

    C++

---
+++ C++ Programs/Structs.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct Student
+{
+    unsigned age{};
+    std::string first_name{};
+    std::string last_name{};
+    unsigned standard{};
+};
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
+
