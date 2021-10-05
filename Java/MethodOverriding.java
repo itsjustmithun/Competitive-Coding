6e7b7d1ae105bf88eedcdbc61a4391341a74f9f4
Ashutosh Pandey <81689972+Ashutoshpandey-coder@users.noreply.github.com>
2021-10-06 00:42:40 +0530

    Method overriding hackerrank

---
+++ Java/MethodOverriding.java
@@ -0,0 +1,87 @@
+/**
+* Contributor🎅
+* Name: Ashutoshpandey-coder (Ashutosh pandey)
+* Github: https://github.com/Ashutoshpandey-coder
+*/
+
+/**CONCEPT */
+/**
+ * When a subclass inherits from a superclass, it also inherits its methods; 
+ * however, it can also override the superclass methods (as well as declare and implement new ones).
+ *  Consider the following Sports class:
+ * 
+  class Sports{
+    String getName(){
+            return "Generic Sports";
+        }
+        void getNumberOfTeamMembers(){
+            System.out.println( "Each team has n players in " + getName() );
+        }
+    }
+    * Next, we create a Soccer class that inherits from the Sports class. We can override the getName method 
+    * and return a different, subclass-specific string:
+        class Soccer extends Sports{
+            @Override
+            String getName(){
+                return "Soccer Class";
+            }
+        }
+ */
+/**NOTE :  When overriding a method, you should precede it with the
+ *  @Override annotation. The parameter(s) and return type of an 
+ * overridden method must be exactly the same as those of the method inherited from the supertype. 
+ *  */
+
+/**TASK */
+/**
+ * Complete the code in your editor by writing an overridden getNumberOfTeamMembers method that prints the same statement as the superclass' getNumberOfTeamMembers method,
+ *  except that it replaces n with 11 (the number of players on a Soccer team). 
+ */
+
+ /**OUTPUT
+  * When executed, your completed code should print the following:
+    Generic Sports
+    Each team has n players in Generic Sports
+    Soccer Class
+    Each team has 11 players in Soccer Class
+  */
+
+import java.util.*;
+//Parent class with two methods with body
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+//Child class overriding both methods of parent class.
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+    // Write your overridden getNumberOfTeamMembers method here
+    @Override
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has 11 players in " + getName() );
+    }
+    
+
+}
+public class MethodOverriding {
+    public static void main(String[] args) {
+        //object of Sports class.
+        Sports c1 = new Sports();
+        //object of Soccer class;
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+    }
+    
+}
